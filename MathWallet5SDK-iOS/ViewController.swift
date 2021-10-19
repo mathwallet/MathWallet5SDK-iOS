a3768e5f65d30169237c12a0e25e0cdfb3bb1539
Forrest <zyyu1990@gmail.com>
2021-10-19 12:01:22 +0800

    Initial Commit

---
+++ MathWallet5SDK-iOS/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MathWallet5SDK-iOS
+//
+//  Created by math on 2021/10/19.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
